a3bf23301401754b49fc0933f4c61d0f1864f1aa
Flo <florent.vouin@gmail.com>
2022-12-21 16:27:50 +0100

    📝 Document the permissions_group resource

---
+++ examples/resources/metabase_permissions_group/import.sh
@@ -0,0 +1,2 @@
+# Use the integer ID from the Metabase API.
+terraform import metabase_permissions_group.group 1
---
+++ examples/resources/metabase_permissions_group/resource.tf
@@ -0,0 +1,3 @@
+resource "metabase_permissions_group" "data_analysts" {
+  name = "🧑‍🔬 Data Analysts"
+}
