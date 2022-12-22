e610e8f06d97eae43f39ef90b77604ce11406910
Flo <florent.vouin@gmail.com>
2022-12-22 11:03:09 +0100

    📝 Document the permissions_graph resource

---
+++ examples/resources/metabase_permissions_graph/import.sh
@@ -0,0 +1,3 @@
+# By convention, the revision number of the permissions graph should be used, although it does not really matter as it
+# will be read during the import anyway.
+terraform import metabase_permissions_graph.graph 1
