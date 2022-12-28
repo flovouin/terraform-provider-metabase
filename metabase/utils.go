44f5e1aaa3f4aa73b69b67b3b15594245dc6e1ef
Flo <florent.vouin@gmail.com>
2022-12-28 07:29:07 +0100

    ✨ Implement field reference replacement logic

--- metabase/utils.go
+++ metabase/utils.go
@@ -2,3 +2,6 @@ package metabase
 
 // The default ID of the `Administrators` permissions group, created automatically by Terraform.
 const AdministratorsPermissionsGroupId = 2
+
+// The name of the literal in an array, indicating a reference to a `Field` object.
+const FieldLiteral = "field"
